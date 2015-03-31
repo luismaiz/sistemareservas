d277315d6b57ba9d6f8846d941537d01b5cea9b7
Luis <Luis@Luis-HP>
2015-03-31 20:43:30 +0200

    Primera version

---
+++ Negocio/AccesoDatos/AdministradorDAO.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AdministradorDAO
+ *
+ * @author Luis
+ */
+class AdministradorDAO {
+    //put your code here
+}
