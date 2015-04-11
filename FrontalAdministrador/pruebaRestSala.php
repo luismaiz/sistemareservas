5e8249e8b847e7b2c8d53af3a57d8cae669ec4de
Mariete <Mariete@Mariete-PC>
2015-04-11 12:00:03 +0200

    mariosgsg: modificaciones

--- FrontalAdministrador/pruebaRestSala.php
+++ FrontalAdministrador/pruebaRestSala.php
@@ -1,184 +1,202 @@
 <html>
-<head>
-<script>
-function AjaxObj()
-{
-  var xmlhttp = null;
+    <head>
+        <script>
+            function AjaxObj()
+            {
+                var xmlhttp = null;
 
-  if (window.XMLHttpRequest)
-  {
-    xmlhttp = new XMLHttpRequest();
+                if (window.XMLHttpRequest)
+                {
+                    xmlhttp = new XMLHttpRequest();
 
-    if (xmlhttp.overrideMimeType)
-    {
-      xmlhttp.overrideMimeType('text/xml');
-    }
-  }
-  else if (window.ActiveXObject)
-  { 
-    // Internet Explorer    
-    try
-    {
-      xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-    }
-    catch (e)
-    {
-      try
-      {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      catch (e)
-      {
-        xmlhttp = null;
-      }
-    }
+                    if (xmlhttp.overrideMimeType)
+                    {
+                        xmlhttp.overrideMimeType('text/xml');
+                    }
+                }
+                else if (window.ActiveXObject)
+                {
+                    // Internet Explorer    
+                    try
+                    {
+                        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+                    }
+                    catch (e)
+                    {
+                        try
+                        {
+                            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+                        }
+                        catch (e)
+                        {
+                            xmlhttp = null;
+                        }
+                    }
 	
-    if (!xmlhttp && typeof XMLHttpRequest!='undefined')
-    {
-      xmlhttp = new XMLHttpRequest();
+                    if (!xmlhttp && typeof XMLHttpRequest!='undefined')
+                    {
+                        xmlhttp = new XMLHttpRequest();
 	  
-      if (!xmlhttp)
-      {
-        failed = true;
-      }
-    }
-  }
-  return xmlhttp;
-}
+                        if (!xmlhttp)
+                        {
+                            failed = true;
+                        }
+                    }
+                }
+                return xmlhttp;
+            }
 
-var Ajax = new AjaxObj();
+            var Ajax = new AjaxObj();
 
-function crearSala() {
-	var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=crearSala";		
-	var Params = 'idSala='+ document.getElementById('idSala').value +
-		     '&Nombre='+ document.getElementById('Nombre').value + 
-		     '&Capacidad='+ document.getElementById('Capacidad').value + 
-		     '&Descripcion='+ document.getElementById('Descripcion').value;
+            function crearSala() {
+                var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=crearSala";		
+                var Params = 'idSala='+ document.getElementById('idSala').value +
+                    '&NombreSala='+ document.getElementById('NombreSala').value +
+                    '&CapacidadSala='+ document.getElementById('CapacidadSala').value +
+                    '&DescripcionSala='+ document.getElementById('DescripcionSala').value +
+                    '&FechaAlta='+ document.getElementById('FechaAlta').value +
+                    '&FechaBaja='+ document.getElementById('FechaBaja').value;
 
-	alert(Params);
+                alert(Params);
 	
 	
-	Ajax.open("POST", Url, true);
-	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
-	Ajax.send(Params); // Enviamos los datos
-}
+                Ajax.open("POST", Url, true);
+                Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                Ajax.send(Params); // Enviamos los datos
+            }
 
 
-function actualizarSala() {	
-	var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=actualizarSala";		
-	var Params = 'idSala='+ document.getElementById('idSala').value +
-		     '&Nombre='+ document.getElementById('Nombre').value + 
-		     '&Capacidad='+ document.getElementById('Capacidad').value + 
-		     '&Descripcion='+ document.getElementById('Descripcion').value;
-
-	
-	Ajax.open("PUT", Url, false);
-	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
-	Ajax.send(Params); // Enviamos los datos
-}
-
-function borrarSala() {	
-	var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=borrarSala";
-	var Params = 'idSala='+ document.getElementById('idSala').value;
+            function actualizarSala() {
+                var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=actualizarSala";
+                var Params = 'idSala='+ document.getElementById('idSala').value +
+                    '&NombreSala='+ document.getElementById('NombreSala').value +
+                    '&CapacidadSala='+ document.getElementById('CapacidadSala').value +
+                    '&DescripcionSala='+ document.getElementById('DescripcionSala').value +
+                    '&FechaAlta='+ document.getElementById('FechaAlta').value +
+                    '&FechaBaja='+ document.getElementById('FechaBaja').value;
 
 	
-	Ajax.open("DELETE", Url, false);
-	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
-	Ajax.send(Params); // Enviamos los datos
-}
+                Ajax.open("PUT", Url, false);
+                Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                Ajax.send(Params); // Enviamos los datos
+            }
 
-function obtenerSalas() {	
-	//var Url = "http://www.rightwatch.es/pfgreservas/Api.php?url=obtenerSalas";		
-	var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=obtenerSalas";
-	var Params = '';
+            function borrarSala() {
+                var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=borrarSala";
+                var Params = 'idSala='+ document.getElementById('idSala').value;
 
 	
-	Ajax.open("GET", Url, true);
-	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
-	Ajax.send(Params); // Enviamos los datos
+                Ajax.open("DELETE", Url, false);
+                Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                Ajax.send(Params); // Enviamos los datos
+            }
+
+            function obtenerSalas() {
+                //var Url = "http://www.rightwatch.es/pfgreservas/Api.php?url=obtenerSalas";
+                var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=obtenerSalas";
+                var Params = '';
+
 	
-	var RespTxt = Ajax.responseText;	
+                Ajax.open("GET", Url, false);
+                Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                Ajax.send(Params); // Enviamos los datos
 	
-	var Clase = eval('(' + RespTxt + ')');	
+                var RespTxt = Ajax.responseText;
+        
+                alert(RespTxt);
 	
-	var contenido = "";		    
-	var div = document.getElementById("salas");
-	contenido = contenido + '<form>';
+                var Clase = eval('(' + RespTxt + ')');	
+        
+                alert(Clase);
 	
-	for(i=0; i<Clase.salas.length; i++){		
-		contenido = contenido + 'idSala: <input type="text" id="idSala" name="idSala" value="' + Clase.salas[i].idSala + '"></input>';
-		contenido = contenido + 'Nombre: <input type="text" id="Nombre" name="Nombre" value="' + Clase.salas[i].Nombre + '"></input>';
-		contenido = contenido + 'Capacidad: <input type="text" id="Capacidad" name="Capacidad" value="' + Clase.salas[i].Capacidad + '"></input>';
-		contenido = contenido + 'Descripcion: <input type="text" id="Descripcion" name="Descripcion" value="' + Clase.salas[i].Descripcion + '"></input>';
-		contenido = contenido + "</br>";
-	}
-	contenido = contenido + '</form>';
+                var contenido = "";
+                var div = document.getElementById("salas");
+                contenido = contenido + '<form>';
 	
-	div.innerHTML = contenido;
+                for(i=0; i<Clase.salas.length; i++){
+                    contenido = contenido + 'idSala: <input type="text" id="idSala" name="idSala" value="' + Clase.salas[i].idSala + '"></input>';
+                    contenido = contenido + 'NombreSala: <input type="text" id="NombreSala" name="NombreSala" value="' + Clase.salas[i].NombreSala + '"></input>';
+                    contenido = contenido + 'CapacidadSala: <input type="text" id="CapacidadSala" name="CapacidadSala" value="' + Clase.salas[i].CapacidadSala + '"></input>';
+                    contenido = contenido + 'DescripcionSala: <input type="text" id="DescripcionSala" name="DescripcionSala" value="' + Clase.salas[i].DescripcionSala + '"></input>';
+                    contenido = contenido + 'FechaAlta: <input type="text" id="FechaAlta" name="FechaAlta" value="' + Clase.salas[i].FechaAlta + '"></input>';
+                    contenido = contenido + 'FechaBaja: <input type="text" id="FechaBaja" name="FechaBaja" value="' + Clase.salas[i].FechaBaja + '"></input>';                    
+                    contenido = contenido + "<br>";
+                }
+                contenido = contenido + '</form>';
 	
-	  //alert('Estado: '+ Clase.estado);
-	  //alert('idSala: '+ Clase.salas.length);
-	  //alert('idSala: '+ Clase.salas[0].idSala);
-	  //alert('Nombre: '+ Clase.salas[0].Nombre);
-	  //alert('Capacidad: '+ Clase.salas[0].Capacidad);
-	  //alert('Descripcion: '+ Clase.salas[0].Descripcion);
+                div.innerHTML = contenido;
+	
+                //alert('Estado: '+ Clase.estado);
+                //alert('idSala: '+ Clase.salas.length);
+                //alert('idSala: '+ Clase.salas[0].idSala);
+                //alert('NombreSala: '+ Clase.salas[0].NombreSala);
+                //alert('CapacidadSala: '+ Clase.salas[0].CapacidadSala);
+                //alert('DescripcionSala: '+ Clase.salas[0].DescripcionSala);
 	  
-	  //document.getElementById('idSala').value=Clase.salas[0].idSala;
-	  //document.getElementById('Nombre').value=Clase.salas[0].Nombre;
-	  //document.getElementById('Capacidad').value=Clase.salas[0].Capacidad;
-	  //document.getElementById('Descripcion').value=Clase.salas[0].Descripcion;
-}
+                //document.getElementById('idSala').value=Clase.salas[0].idSala;
+                //document.getElementById('NombreSala').value=Clase.salas[0].NombreSala;
+                //document.getElementById('CapacidadSala').value=Clase.salas[0].CapacidadSala;
+                //document.getElementById('DescripcionSala').value=Clase.salas[0].DescripcionSala;
+            }
 
-function obtenerSala() {	
-        var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=obtenerSala";		
-	var Params = 'idSala='+ document.getElementById('idSala').value;
+            function obtenerSala() {
+                var Url = "http://localhost/sistemareservas/Negocio/NegocioAdministrador/AdministradorBO.php?url=obtenerSala";
+                var Params = 'idSala='+ document.getElementById('idSala').value;
 
 	
-	Ajax.open("POST", Url, false);
-	Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");	
-	Ajax.send(Params); // Enviamos los datos
+                Ajax.open("POST", Url, false);
+                Ajax.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+                Ajax.send(Params); // Enviamos los datos
 	
-	var RespTxt = Ajax.responseText;	
+                var RespTxt = Ajax.responseText;
 	
-	alert("Resultado:" + RespTxt);
+                alert("Resultado:" + RespTxt);
 	
-	alert(eval('(' + RespTxt + ')'));
-	var Clase = JSON.parse(RespTxt);
+                //alert(eval('(' + RespTxt + ')'));
+                //alert("Prueba: " + $.parseJSON(RespTxt));
+                //var Clase = eval('(' + RespTxt + ')');
+                var Clase = eval('(' + RespTxt + ')');	
 	
-            //eval('(' + RespTxt + ')');
-	  alert('Estado: '+ Clase.estado);
-	  alert('idSala: '+ Clase.sala.idSala);
-	  alert('Nombre: '+ Clase.sala.Nombre);
-	  alert('Capacidad: '+ Clase.sala.Capacidad);
-	  alert('Descripcion: '+ Clase.sala.Descripcion);
+                //eval('(' + RespTxt + ')');
+                alert(Clase);
+                alert('Estado: '+ Clase.estado);
+                alert('idSala: '+ Clase.sala.idSala);
+                alert('NombreSala: '+ Clase.sala.NombreSala);
+                alert('CapacidadSala: '+ Clase.sala.CapacidadSala);
+                alert('DescripcionSala: '+ Clase.sala.DescripcionSala);
 	  
-	  document.getElementById('idSala').value=Clase.sala.idSala;
-	  document.getElementById('Nombre').value=Clase.sala.Nombre;
-	  document.getElementById('Capacidad').value=Clase.sala.Capacidad;
-	  document.getElementById('Descripcion').value=Clase.sala.Descripcion;
-}
+                document.getElementById('idSala').value=Clase.sala.idSala;
+                document.getElementById('NombreSala').value=Clase.sala.NombreSala;
+                document.getElementById('CapacidadSala').value=Clase.sala.CapacidadSala;
+                document.getElementById('DescripcionSala').value=Clase.sala.DescripcionSala;
+                document.getElementById('FechaAlta').value=Clase.sala.FechaAlta;
+                document.getElementById('FechaBaja').value=Clase.sala.FechaBaja;                
+            }
 
-</script>
-</head>
-<body>
+        </script>
+    </head>
+    <body>
 
-    <form method="post">
-  idSala: <input type="text" id="idSala" name="idSala"></input>
-  Nombre: <input type="text" id="Nombre" name="Nombre"></input>
-  Capacidad: <input type="text" id="Capacidad" name="Capacidad"></input>
-  Descripcion: <input type="text" id="Descripcion" name="Descripcion"></input> 
-  </br></br>
-  <input type="button" value="Crear Sala" onclick="crearSala()"></input>
-  <input type="button" value="Obtener Sala" onclick="obtenerSala()"></input>  
-  <input type="button" value="Actualizar Sala" onclick="actualizarSala()"></input>  
-  <input type="button" value="Borrar Sala" onclick="borrarSala()"></input>  
-</form>
-
-
-<input type="button" value="Obtener Salas" onclick="obtenerSalas()"></input>
-<div id="salas">
-</div>
+        <form method="post">
+            idSala: <input type="text" id="idSala" name="idSala"></input>
+            NombreSala: <input type="text" id="NombreSala" name="NombreSala"></input>
+            CapacidadSala: <input type="text" id="CapacidadSala" name="CapacidadSala"></input>
+            DescripcionSala: <input type="text" id="DescripcionSala" name="DescripcionSala"></input>
+            FechaAlta: <input type="text" id="FechaAlta" name="FechaAlta"></input>
+            FechaBaja: <input type="text" id="FechaBaja" name="FechaBaja"></input>
+            
+            </br></br>
+            <input type="button" value="Crear Sala" onclick="crearSala()"></input>
+            <input type="button" value="Obtener Sala" onclick="obtenerSala()"></input>
+            <input type="button" value="Actualizar Sala" onclick="actualizarSala()"></input>
+            <input type="button" value="Borrar Sala" onclick="borrarSala()"></input>
+        </form>
 
 
-</body>
+        <input type="button" value="Obtener Salas" onclick="obtenerSalas()"/>
+        <div id="salas">
+        </div>
+
+
+    </body>
 </html>
