7b5fad8ec66f1b378af4099f178cb1560fda2ad7
Luis <Luis@Luis-HP>
2015-04-13 20:33:45 +0200

    Version PHP

--- FrontalAdministrador/headerReservas.php
+++ FrontalAdministrador/headerReservas.php
@@ -98,13 +98,11 @@ function AjaxObj()
             <!-- user dropdown starts -->
             <div class="btn-group pull-right">
                 <button class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-                    <i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs"> admin</span>
+                    <i class="glyphicon glyphicon-user"></i><span class="hidden-sm hidden-xs"> administrador</span>
                     <span class="caret"></span>
                 </button>
                 <ul class="dropdown-menu">
-                    <li><a href="#">Profile</a></li>
-                    <li class="divider"></li>
-                    <li><a href="login.html">Logout</a></li>
+                    <li><a href="login.html">Cerrar sesion</a></li>
                 </ul>
             </div>
             <!-- user dropdown ends -->
